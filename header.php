ae946ce3f3389efefcb29a863aabd61487bf3d7c
MessaoudBadreddine <messaoud.bedreddine1@gmail.com>
2023-03-23 10:18:06 +0000

    First commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<META http-equiv=Content-Type content="text/html; charset=utf-8">
+
+    <head>
+    
+        <title>Add, Edit, Delete In PHP/MySQL Using PDO</title>
+
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" >
+     
+        <link rel="stylesheet" type="text/css" href="css/DT_bootstrap.css">
+		
+        <link href="modal/css1/bootstrap1.css" rel="stylesheet" type="text/css" >
+     
+
+
+</head>
+<script src="modal/js1/jquery1.js" type="text/javascript"></script>
+<script src="modal/js1/bootstrap1.js" type="text/javascript"></script>
+
+
+
+<script src="js/jquery.js" type="text/javascript"></script>
+<script src="js/bootstrap.js" type="text/javascript"></script>
+
+<script type="text/javascript" charset="utf-8" language="javascript" src="js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf-8" language="javascript" src="js/DT_bootstrap.js"></script>
